c9e27218d56de92ce978641623ff0216d4023e1b
network7277 <husanjurayev7277@gmail.com>
2023-08-14 15:47:24 +0500

    screenOne, splash and screenTwo aded

---
+++ lib/screens/screen_one.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:laza_shop_clone/widgets/gender_selection.dart';
+
+class ScreenOne extends StatefulWidget {
+  const ScreenOne({super.key});
+
+  @override
+  State<ScreenOne> createState() => _ScreenOneState();
+}
+
+class _ScreenOneState extends State<ScreenOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF9775FA),
+      body: Stack(
+        children: [
+          Image.asset('assets/person.png'),
+          Positioned(
+            left: 15,
+            right: 15,
+            bottom: 15,
+            child: Container(
+              height: 244,
+              width: 345,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10))
+              ),
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 25),
+                    child: Text('Look Good, Feel Good', style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600, color: Color(0xFF1D1E20))),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text('Create your individual & unique style and\n look amazing everyday.', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Color(0xFF8F959E))),
+                  const SizedBox(height: 20),
+                  const GenderSelection(),
+                  const SizedBox(height: 10),
+                  TextButton(
+                    onPressed: (){}, 
+                    child: const Text(
+                      'Skip',
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500, color: Color(0xFF8F959E)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/gender_selection.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class GenderSelection extends StatefulWidget {
+  const GenderSelection({super.key});
+
+  @override
+  State<GenderSelection> createState() => _GenderSelectionState();
+}
+
+class _GenderSelectionState extends State<GenderSelection> {
+  int current = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          genderSelection('Men', 0),
+          genderSelection('Women', 1)
+        ],
+      ),
+    );
+  }
+
+
+  Widget genderSelection(String gender, int index){
+    return InkWell(
+      onTap: (){
+        setState(() {
+          current = index;
+        });
+      },
+      child: Container(
+        width: 152,
+        height: 60,
+        decoration: BoxDecoration(
+          color: current == index? const Color(0xFF9775FA): const Color(0xFFF5F6FA),
+          borderRadius: const BorderRadius.all(Radius.circular(10))
+        ),
+        child: Center(
+          child: Text(
+            gender,
+            style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500, color: current == index? Colors.white: const Color(0xFF8F959E)),
+          ),
+        ),
+      ),
+    );
+  }
+}
